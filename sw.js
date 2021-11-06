1053ebd38c5f4c013a8b0b7f70432dd5d05e0364
latteslurp <55607218+latteslurp@users.noreply.github.com>
2021-11-06 10:07:17 -0700

    with service worker

--- sw.js
+++ sw.js
@@ -2,58 +2,58 @@
 //         so do not move it next to the other scripts
 
 const CACHE_NAME = 'lab-7-starter';
-// const urlsToCache = [
-//   'assets/scripts/main.js',
-//   'assets/scripts/Router.js',
-//   'assets/styles/main.css'
-// ];
+const urlsToCache = [
+  'assets/scripts/main.js',
+  'assets/scripts/Router.js',
+  'assets/styles/main.css'
+];
 
-// // Once the service worker has been installed, feed it some initial URLs to cache
-// self.addEventListener('install', function (event) {
-//   /**
-//    * TODO - Part 2 Step 2
-//    * Create a function as outlined above
-//    */
-//   // Perform install steps
-//   event.waitUntil(
-//     caches.open(CACHE_NAME)
-//       .then(function(cache) {
-//         console.log('Opened cache');
-//         return cache.addAll(urlsToCache);
-//       })
-//   );
+// Once the service worker has been installed, feed it some initial URLs to cache
+self.addEventListener('install', function (event) {
+  /**
+   * TODO - Part 2 Step 2
+   * Create a function as outlined above
+   */
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 
-// });
+});
 
-// /**
-//  * Once the service worker 'activates', this makes it so clients loaded
-//  * in the same scope do not need to be reloaded before their fetches will
-//  * go through this service worker
-//  */
-// self.addEventListener('activate', function (event) {
-//   /**
-//    * TODO - Part 2 Step 3
-//    * Create a function as outlined above, it should be one line
-//    */
+/**
+ * Once the service worker 'activates', this makes it so clients loaded
+ * in the same scope do not need to be reloaded before their fetches will
+ * go through this service worker
+ */
+self.addEventListener('activate', function (event) {
+  /**
+   * TODO - Part 2 Step 3
+   * Create a function as outlined above, it should be one line
+   */
   
-//   event.waitUntil(clients.claim());
+  event.waitUntil(clients.claim());
 
-// });
+});
 
-// // Intercept fetch requests and store them in the cache
-// self.addEventListener('fetch', function (event) {
-//   /**
-//    * TODO - Part 2 Step 4
-//    * Create a function as outlined above
-//    */
-//    event.respondWith(
-//     caches.match(event.request)
-//       .then(function(response) {
-//         // Cache hit - return response
-//         if (response) {
-//           return response;
-//         }
-//         return fetch(event.request);
-//       })
-//     );
-// });
+// Intercept fetch requests and store them in the cache
+self.addEventListener('fetch', function (event) {
+  /**
+   * TODO - Part 2 Step 4
+   * Create a function as outlined above
+   */
+   event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+    );
+});
